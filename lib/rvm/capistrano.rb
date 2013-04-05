8049eff86ecfcde19beba0f8b54c3e5b3f051574
Vish Vishvanath <vish.vishvanath@gmail.com>
2013-04-05 11:58:10 +0100

    Adding a little tweak to allow the emptying of gemsets

--- lib/rvm/capistrano.rb
+++ lib/rvm/capistrano.rb
@@ -236,8 +236,22 @@ module Capistrano
   end if const_defined? :Configuration
 end
 
-# E.g, to use ree and rails 3:
-#
-#   require 'rvm/capistrano'
-#   set :rvm_ruby_string, "ree@rails3"
-#
+# Extension to empty gemset.
+
+module Capistrano extend self
+  Configuration.instance(true).load do
+    namespace :rvm do
+      desc "Empty gemset"
+      task :empty_gemset do
+        ruby, gemset = rvm_ruby_string.to_s.strip.split /@/
+        if %w( release_path default ).include? "#{ruby}"
+          raise "gemset can not be emptied when using :rvm_ruby_string => :#{ruby}"
+        else
+          if gemset
+            run "#{File.join(rvm_bin_path, "rvm")} #{ruby} do rvm --force gemset empty #{gemset}", :shell => "#{rvm_install_shell}"
+          end
+        end
+      end
+    end
+  end if const_defined? :Configuration
+end
