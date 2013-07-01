bf591fd15394e3f8e0adbb1de99340bf5313d335
Jordon Bedwell <envygeeks@gmail.com>
2013-07-01 17:40:07 -0500

    Cleanup.

--- lib/rvm/capistrano/helpers/base.rb
+++ lib/rvm/capistrano/helpers/base.rb
@@ -2,7 +2,8 @@ module Capistrano
 end
 
 def rvm_with_capistrano(&block)
-  if Capistrano.const_defined? :Configuration and Capistrano::Configuration.methods.map(&:to_sym).include? :instance
+  if Capistrano.const_defined?(:Configuration) &&
+      Capistrano::Configuration.methods.map(&:to_sym).include?(:instance)
     Capistrano::Configuration.instance(true).load(&block)
   end
 end
